d85fc706502d57002fa5b4c62af5536c4a634e86
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-25 13:18:15 +0500

    передача props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import CostItem from "./components/CostItem";
 
 function App() {
+  const costs = [
+    {
+      date: new Date(2021, 10, 22),
+      description: "Товар 1",
+      amount: 100,
+    },
+    {
+      date: new Date(2022, 5, 12),
+      description: "Товар 2",
+      amount: 200,
+    },
+    {
+      date: new Date(),
+      description: "Товар 3",
+      amount: 300,
+    },
+  ];
+
   return (
     <div>
       <h1>Hello React!</h1>
-      <CostItem></CostItem>
+      <CostItem
+        date={costs[0].date}
+        description={costs[0].description}
+        amount={costs[0].amount}
+      ></CostItem>
+      <CostItem
+        date={costs[1].date}
+        description={costs[1].description}
+        amount={costs[1].amount}
+      ></CostItem>
+      <CostItem
+        date={costs[2].date}
+        description={costs[2].description}
+        amount={costs[2].amount}
+      ></CostItem>
     </div>
   );
 }
