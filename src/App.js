fa262ce4e7df743e0400cd9b12f57541c8343c5f
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-25 21:14:46 +0500

    организация components и arrow func

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Costs from "./components/Costs";
+import Costs from "./components/Costs/Costs";
 
-function App() {
+const App = () => {
   const costs = [
     {
       date: new Date(2021, 10, 22),
@@ -25,6 +25,6 @@ function App() {
       <Costs costs={costs} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,8 +1,8 @@
 import "./Costs.css";
 import CostItem from "./CostItem";
-import Card from "./Card";
+import Card from "../UI/Card";
 
-function Costs(props) {
+const Costs = (props) => {
   return (
     <Card className="costs">
       <CostItem
@@ -22,6 +22,6 @@ function Costs(props) {
       />
     </Card>
   );
-}
+};
 
 export default Costs;
