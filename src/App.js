eb7718695bc542ab8e5d206f042116a2a0757425
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-26 19:39:03 +0500

    коммуникация компонентов от наследника к родителю (снизу вверх)

--- src/App.js
+++ src/App.js
@@ -7,22 +7,30 @@ const App = () => {
       date: new Date(2021, 10, 22),
       description: "Товар 1",
       amount: 100,
+      id: "1",
     },
     {
       date: new Date(2022, 5, 12),
       description: "Товар 2",
       amount: 200,
+      id: "2",
     },
     {
       date: new Date(),
       description: "Товар 3",
       amount: 300,
+      id: "3",
     },
   ];
 
+  const addCostHandler = (cost) => {
+    console.log(cost);
+    console.log("App");
+  };
+
   return (
     <div>
-      <NewCost />
+      <NewCost onAddCost={addCostHandler} />
       <Costs costs={costs} />
     </div>
   );
