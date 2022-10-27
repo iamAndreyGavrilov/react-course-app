79d9f9c2c9af6e1e9b38bf01c9c692ff109ee598
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-27 14:59:53 +0500

    использование списков с состоянием

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import NewCost from "./components/NewCost/NewCost";
 import Costs from "./components/Costs/Costs";
+import { useState } from "react";
+
+const INITIAL_COSTS = [
+  {
+    date: new Date(2021, 10, 22),
+    description: "Товар 1",
+    amount: 100,
+    id: "1",
+  },
+  {
+    date: new Date(2022, 5, 12),
+    description: "Товар 2",
+    amount: 200,
+    id: "2",
+  },
+  {
+    date: new Date(),
+    description: "Товар 3",
+    amount: 300,
+    id: "3",
+  },
+];
 
 const App = () => {
-  const costs = [
-    {
-      date: new Date(2021, 10, 22),
-      description: "Товар 1",
-      amount: 100,
-      id: "1",
-    },
-    {
-      date: new Date(2022, 5, 12),
-      description: "Товар 2",
-      amount: 200,
-      id: "2",
-    },
-    {
-      date: new Date(),
-      description: "Товар 3",
-      amount: 300,
-      id: "3",
-    },
-  ];
+  const [costs, setCosts] = useState(INITIAL_COSTS);
 
   const addCostHandler = (cost) => {
-    console.log(cost);
-    console.log("App");
+    setCosts((previousStateCosts) => {
+      return [cost, ...previousStateCosts];
+    });
   };
 
   return (
